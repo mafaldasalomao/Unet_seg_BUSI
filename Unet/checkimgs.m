5502c250314110b36f0a50749834446611c07fa0
salomao-mafalda <salomaomachadon@gmail.com>
2022-09-30 20:21:49 -0500

    Upload more data to train

--- Unet/checkimgs.m
+++ Unet/checkimgs.m
@@ -7,7 +7,7 @@ imds = imageDatastore(dataset_base);
 %%
 for i =1:length(imds.Files)
     img = readimage(imds,i);
-    %img = imresize(img, [224 224]);
+    img = imresize(img, [224 224]);
     [~, ~, c] = size(img);
     img = rescale(img, 0, 1);
     imwrite(img, imds.Files{i});
